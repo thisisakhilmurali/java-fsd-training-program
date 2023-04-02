4aa6fb4638bbff545b7e5a1f5b20afdca5ade496
akhil <akhilmuraliai21@gmail.com>
2023-04-02 22:44:08 +0530

    Multithreading in java Added

--- Java/multithreading/ThreadRunnableInterface.java
+++ Java/multithreading/ThreadRunnableInterface.java
@@ -1,5 +1,22 @@
 package multithreading;
 
+
+
 public class ThreadRunnableInterface {
+
+    public static void main(String[] args) {
+
+        System.out.println("Main Thread: " + Thread.currentThread().getName());
+
+        Thread thread = new Thread(new RunnableInterfaceDemo());
+        thread.start();
+    }
+
+    private static class RunnableInterfaceDemo implements Runnable {
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + ", executing run() method.");
+        }
+    }
 
 }
