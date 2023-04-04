f12bda45200e184ddd3e87f2f9262127896f45a1
akhil <akhilmuraliai21@gmail.com>
2023-04-04 19:05:39 +0530

    Spring Validation and Exception Handling (minor fix)

--- spring/validation-exception-handling-main/src/main/java/com/javatechie/api/advice/ApplicationExceptionHandler.java
+++ spring/validation-exception-handling-main/src/main/java/com/javatechie/api/advice/ApplicationExceptionHandler.java
@@ -14,7 +14,6 @@ import java.util.Map;
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
 
-
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex) {
--- spring/validation-exception-handling-main/src/main/java/com/javatechie/api/service/UserService.java
+++ spring/validation-exception-handling-main/src/main/java/com/javatechie/api/service/UserService.java
@@ -11,10 +11,8 @@ import java.util.List;
 
 @Service
 public class UserService {
-
     @Autowired
     private UserRepository repository;
-
 
     public User saveUser(UserRequest userRequest) {
         User user = User.
